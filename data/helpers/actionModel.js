ee2f29abe4f758b27cbedc20fe67aa061a29bbce
Victor <victor.tran@tempestas.io>
2020-05-17 22:38:46 -0400

    update helpers because lambda's suck

--- data/helpers/actionModel.js
+++ data/helpers/actionModel.js
@@ -2,46 +2,51 @@ const db = require('../dbConfig.js');
 const mappers = require('./mappers');
 
 module.exports = {
-  get,
-  insert,
-  update,
-  remove,
+    get,
+    insert,
+    update,
+    remove,
+    findById,
 };
 
-function get(id) {
-  let query = db('actions');
+function findById(id) {
+    return db('actions').where({ id }).first();
+}
 
-  if (id) {
-    return query
-      .where('id', id)
-      .first()
-      .then((action) => {
-        if (action) {
-          return mappers.actionToBody(action);
-        } else {
-          return null;
-        }
-      });
-  } else {
-    return query.then((actions) => {
-      return actions.map((action) => mappers.actionToBody(action));
-    });
-  }
+function get(id) {
+    let query = db('actions');
+
+    if (id) {
+        return query
+            .where('id', id)
+            .first()
+            .then((action) => {
+                if (action) {
+                    return mappers.actionToBody(action);
+                } else {
+                    return null;
+                }
+            });
+    } else {
+        return query.then((actions) => {
+            return actions.map((action) => mappers.actionToBody(action));
+        });
+    }
 }
 
 function insert(action) {
-  return db('actions')
-    .insert(action, 'id')
-    .then(([id]) => get(id));
+    return db('actions')
+        .insert(action, 'id')
+        .then(([id]) => findById(id));
 }
 
 function update(id, changes) {
-  return db('actions')
-    .where('id', id)
-    .update(changes)
-    .then((count) => (count > 0 ? get(id) : null));
+    return db('actions')
+        .where('id', id)
+        .update(changes)
+        .then((count) => (count > 0 ? findById(id) : null));
 }
 
 function remove(id) {
-  return db('actions').where('id', id).del();
+    return db('actions').where('id', id).del();
 }
